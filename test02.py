e0fcea9081b5f6f4dce06eb06babb7aa4b385c5d
JEngHAng <jangg500z@gmail.com>
2023-10-20 10:00:58 +0700

    test02 : STR STR

---
+++ test02.py
@@ -0,0 +1,28 @@
+#character in string
+       # 01234567890123
+infoA = "Hello SAU 2023"
+       # 43210987654321 คือ ติดลบ(-)
+print (infoA[6])
+print (infoA[-8])
+#เข้าถถึงตัวอักษรหลายๆตัวใน string ใช้วิธี slicing ด้วย index number
+
+#SAU
+print (infoA[6:9])
+print (infoA[-8:-5])
+
+#o SAU 20
+print (infoA[4:12])
+
+#string Method
+print (infoA.upper()) #ตัวพิมพ์ใหญ่
+print (infoA.lower()) #ตัวพิมพ์เล็ก
+print (infoA.capitalize()) #
+print (infoA.count("l")) #นับจำนวนตัวอักษรหรือจำนวน
+print (infoA.isdigit()) #นับว่ามีตัวเลขไหม
+print (infoA.islower()) #นับว่ามีตัวพิมพ์เล็กกี่ตัว
+infoB = infoA.replace("SAU","IoT")
+print (infoB)
+print (infoB.replace("Hello","Hi..."))
+
+#String string
+print (len(infoA))
